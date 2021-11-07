5b8eee38a7f5df5cb1c0da62453b74c62ac27669
Fernando Fraga <=>
2021-11-06 23:18:35 -0300

    Estruturacao projeto

---
+++ api/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '18a55dd90d27abc98bb7d4892c0d37639cf90cc9',
+        'name' => '3f/task-manager',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '3f/task-manager' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '18a55dd90d27abc98bb7d4892c0d37639cf90cc9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
